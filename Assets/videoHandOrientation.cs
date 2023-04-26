054ad467ae4b735ac8adecbc9bdc864853038a3c
Andre Dantas <andre@campus-prof04.isd.matriz>
2023-04-26 08:38:14 -0300

    Atualização de giro de camera

--- Assets/videoHandOrientation.cs
+++ Assets/videoHandOrientation.cs
@@ -11,6 +11,7 @@ public class videoHandOrientation : MonoBehaviour
     public string data;
     public float angleXr;
     public float angleXl;
+    public bool isright = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,10 +29,14 @@ public class videoHandOrientation : MonoBehaviour
             acc[0] = acc[0].Replace(".", ",");
             acc[1] = acc[1].Replace(".", ",");
             acc[2] = acc[2].Replace(".", ",");
-            if (topicss.Contains("r"))	
-            	transform.localEulerAngles = new Vector3(float.Parse(acc[0])+angleXr,float.Parse(acc[1]),float.Parse(acc[2]));
-    	    else
-    	        transform.localEulerAngles = new Vector3(float.Parse(acc[0])+angleXl,float.Parse(acc[1]),float.Parse(acc[2]));
+            if (isright)	
+            	transform.localEulerAngles = new Vector3(4.0f*float.Parse(acc[0])+angleXr,float.Parse(acc[1]),float.Parse(acc[2]));
+    	    else{ 
+    	        if (!isright){	
+    	            transform.localEulerAngles = new Vector3(1.0f*(float)Math.Abs(angleXl-float.Parse(acc[0])),float.Parse(acc[1]),float.Parse(acc[2]));
+    	            Debug.Log(Math.Abs(angleXl-float.Parse(acc[0])));
+                }
+            }
             // segmentoDoCorpo.transform.eulerAngles = new Vector3(float.Parse(acc[0])*180f/3.14f,float.Parse(acc[1])*180f/3.14f,(float.Parse(acc[2]))*180f/3.14f);
         }
     }
