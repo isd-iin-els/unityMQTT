39214979a7d9840af4f081604584b4c993b448af
ISDVRTEAM <isdvrteam@gmail.com>
2023-04-26 13:49:50 -0300

    Atualização de scripts
    Teste em colisão, camera, fes etc...

--- Assets/ActionByAngle.cs
+++ Assets/ActionByAngle.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class colisonVest : MonoBehaviour
+public class ActionByAngle : MonoBehaviour
 {
     public GameObject mqttobj;
     private mqttscript mqtt;
     public string vest_topic;
     public int index = 0;
+    public float waitTime = 1.0f;
+    private float timer = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +19,26 @@ public class colisonVest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        timer += Time.deltaTime;
 
-    void OnTriggerEnter(Collider other) { 
+    
+        if (timer > waitTime)
+        {
+        
+
+            // Remove the recorded 2 seconds.
+            timer = timer - waitTime;
+           
+
+
+       if (transform.eulerAngles.x>45) 
+       {
         string json2send = "{\"index\":"+index.ToString()+"}";
         mqtt.publish(vest_topic, json2send);
-        Debug.Log(json2send);  
-    }  
+        Debug.Log(json2send);
+       }   
+    }
+    } 
+    
+    
 }
