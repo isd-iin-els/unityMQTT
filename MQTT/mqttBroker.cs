719f96bfe332ff3626a9e7c15bcbaaee882bfc32
Andre Dantas <andre@campus-prof04.isd.matriz>
2023-09-11 10:06:19 -0300

    Criando broker próprio e salvando log.

--- MQTT/mqttBroker.cs
+++ MQTT/mqttBroker.cs
@@ -1,25 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MQTTnet;
 using MQTTnet.Server;
 using System.Threading.Tasks;
+using System.IO;
 // using MQTTnet.AspNetCore;
 
 public class mqttBroker : MonoBehaviour
 {
     public int port = 1883;
     private IMqttServer mqttServer = null;
+    public string filename = "log.txt";
+    private StreamWriter writer;
     // private Server_ASP_NET_Samples test;
     // private IServiceCollection services;
     // Start is called before the first frame update
     async void Awake()
     {
+    	//writer = new StreamWriter(Application.persistentDataPath + "/" + filename, true);
         var optionsBuilder = new MqttServerOptionsBuilder()
             .WithConnectionBacklog(100)
             .WithDefaultEndpointPort(port);
 
         var mqttServer = new MqttFactory().CreateMqttServer();
+        
+        optionsBuilder.WithApplicationMessageInterceptor(context =>
+	    {
+	    	string data = DateTime.Now.ToString("dd_MM_yyyy_HH:mm:ss") + "; " +  context.ClientId + "; " + context.ApplicationMessage.Topic + "; " + System.Text.Encoding.UTF8.GetString(context.ApplicationMessage.Payload);
+		File.AppendAllText(filename,data + Environment.NewLine);
+			//deserialize directly from that stream.
+			//filestream.WriteLine(data);
+
+	    	
+	    	Debug.Log(data);
+
+	
+	    });
+        
+        
         await mqttServer.StartAsync(optionsBuilder.Build());
     }
 
